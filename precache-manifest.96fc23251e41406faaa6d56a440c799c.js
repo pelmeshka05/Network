515183688ef5ff0e89e05539dffe00c5d063297a
arseniiOsovets <arsenasavets@gmail.com>
2025-01-12 23:35:52 +0300

    Updates

---
+++ precache-manifest.96fc23251e41406faaa6d56a440c799c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962606151a8457a1065470037a77aecf",
+    "url": "/Networkgit/index.html"
+  },
+  {
+    "revision": "115ba52622c31f904df6",
+    "url": "/Networkgit/static/css/2.194f605c.chunk.css"
+  },
+  {
+    "revision": "6d39e2fde192372b2a71",
+    "url": "/Networkgit/static/css/3.05ed161e.chunk.css"
+  },
+  {
+    "revision": "1235235177d4b4acb9a7",
+    "url": "/Networkgit/static/css/4.56fe1bf8.chunk.css"
+  },
+  {
+    "revision": "93cba127121d68377000",
+    "url": "/Networkgit/static/css/main.82da48ea.chunk.css"
+  },
+  {
+    "revision": "115ba52622c31f904df6",
+    "url": "/Networkgit/static/js/2.2d1e5b65.chunk.js"
+  },
+  {
+    "revision": "d091830e8cda01a93b00e7e0ed1d10cc",
+    "url": "/Networkgit/static/js/2.2d1e5b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d39e2fde192372b2a71",
+    "url": "/Networkgit/static/js/3.2ee0f935.chunk.js"
+  },
+  {
+    "revision": "1235235177d4b4acb9a7",
+    "url": "/Networkgit/static/js/4.72591b90.chunk.js"
+  },
+  {
+    "revision": "28c4a59884f72063bc4e",
+    "url": "/Networkgit/static/js/5.94000140.chunk.js"
+  },
+  {
+    "revision": "93cba127121d68377000",
+    "url": "/Networkgit/static/js/main.4c0dca16.chunk.js"
+  },
+  {
+    "revision": "dfaba3985f20a5411de4",
+    "url": "/Networkgit/static/js/runtime-main.dc4367d0.js"
+  },
+  {
+    "revision": "6c3daf6085e46eea1698990ad3578119",
+    "url": "/Networkgit/static/media/preloader.6c3daf60.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/Networkgit/static/media/user.8b1dbb1f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Network/precache-manifest.2aa393cd779fe1c34f6dbcdd98c2d7ca.js"
+  "/Networkgit/precache-manifest.96fc23251e41406faaa6d56a440c799c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Network/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Networkgit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
